ce5d21d6d7c2dec05fe051adbfc11dc37d9da375
Bering Tafa <bering.tafa@teamviewer.com>
2025-10-03 17:19:03 +0200

    footer v1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer className="footer footer-horizontal footer-center bg-primary text-primary-content p-10">
+        <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
             <aside>
                 <svg
                     width="50"
@@ -9,18 +9,16 @@ function Footer() {
                     xmlns="http://www.w3.org/2000/svg"
                     fillRule="evenodd"
                     clipRule="evenodd"
-                    className="inline-block fill-current">
+                    className="fill-current">
                     <path
                         d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
                 </svg>
-                <p className="font-bold">
-                    ACME Industries Ltd.
-                    <br />
-                    Providing reliable tech since 1992
+                <p className="py-5">
+                    <p className="text-xl font-bold">
+                        Prudental
+                    </p>
+                    Providing exceptional dental care with a gentle touch.<br/>Your smile is our priority.
                 </p>
-                <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
-            </aside>
-            <nav>
                 <div className="grid grid-flow-col gap-4">
                     <a>
                         <svg
@@ -56,8 +54,30 @@ function Footer() {
                         </svg>
                     </a>
                 </div>
+            </aside>
+            <nav>
+                <h6 className="footer-title">Quick Access</h6>
+                <a className="link link-hover">About Us</a>
+                <a className="link link-hover">Our Services</a>
+                <a className="link link-hover">Patient Reviews</a>
+                <a className="link link-hover">Book Appointment</a>
+            </nav>
+            <nav>
+                <h6 className="footer-title">Our Services</h6>
+                <a className="link link-hover">General Dentistry</a>
+                <a className="link link-hover">Cosmetic Dentistry</a>
+                <a className="link link-hover">Orthodontics</a>
+                <a className="link link-hover">Dental Implants</a>
+                <a className="link link-hover">Oral Surgery</a>
+                <a className="link link-hover">Emergency Care</a>
+            </nav>
+            <nav>
+                <h6 className="footer-title">Contact Info</h6>
+                <a className="link link-hover">123 Dental Street<br />Health City, HC 12345</a>
+                <a className="link link-hover">(555) 123-4567</a>
+                <a className="link link-hover">info@dentalcare.com</a>
             </nav>
         </footer>
-    );
+    )
 }
 export default Footer;
